0bc5f100b2a9a453f92950466b8f88865e572521
Richard Lyon <richlyon@mac.com>
2024-10-23 17:34:55 +0200

    parallelise hashing

--- src/bin/hashes.rs
+++ src/bin/hashes.rs
@@ -2,9 +2,15 @@
 
 use deduper::{error::AppError, indexer::index_images_in_folder};
 use fjall::{Config, PartitionCreateOptions, PartitionHandle};
-use std::{collections::HashMap, path::PathBuf};
+use indicatif::ProgressStyle;
+use rayon::prelude::*;
+use std::{
+    collections::HashMap,
+    path::PathBuf,
+    sync::{Arc, Mutex},
+};
 
-const _MYLIO_VAULT_ROOT: &str = "/Volumes/SamsungT9/Mylio_22c15a/Mylio Pictures";
+const MYLIO_VAULT_ROOT: &str = "/Volumes/SamsungT9/Mylio_22c15a/Mylio Pictures";
 const TEST_DATA_ROOT: &str = "/Users/richardlyon/dev/deduper/test-data";
 
 fn main() -> Result<(), AppError> {
@@ -12,13 +18,13 @@ fn main() -> Result<(), AppError> {
     let image_hashes =
         keyspace.open_partition("image_hashes", PartitionCreateOptions::default())?;
 
-    // Insert hashes of the images into the fjall keystore
-    let image_paths = index_images_in_folder(&PathBuf::from(TEST_DATA_ROOT));
+    let image_paths = index_images_in_folder(&PathBuf::from(MYLIO_VAULT_ROOT));
+
     let _ = insert_hashes(&image_paths, &image_hashes, false)?;
 
-    // Find duplicate hashes in keystore values
     let duplicates = get_duplicates(&image_hashes)?;
 
+    println!("Duplicates:");
     println!("{:#?}", duplicates);
 
     Ok(())
@@ -29,32 +35,63 @@ fn insert_hashes(
     image_paths: &[PathBuf],
     partition: &PartitionHandle,
     do_clear: bool,
-) -> Result<i32, AppError> {
+) -> Result<(), AppError> {
     if do_clear {
         clear_partition(partition)?;
     }
 
-    let mut insert_count = 0;
+    let pb = indicatif::ProgressBar::new(image_paths.len() as u64);
+    pb.set_style(
+        ProgressStyle::with_template("[{eta_precise}] {bar:40.cyan/blue} {pos:>7}/{len:7} {msg}")
+            .unwrap()
+            .progress_chars("##-"),
+    );
 
-    for image_path in image_paths {
+    let partition = Arc::new(Mutex::new(partition));
+    let pb = Arc::new(Mutex::new(pb));
+
+    image_paths.par_iter().for_each(|image_path| {
         let serialised_image_path = image_path.to_str().unwrap().as_bytes();
 
-        // check if the hash already exists
-        if partition.get(serialised_image_path).unwrap().is_some() {
-            continue;
+        // Check if the hash already exists
+        let exists = {
+            let partition = partition.lock().unwrap();
+            partition.get(serialised_image_path).unwrap().is_some()
+        };
+
+        if exists {
+            return;
         }
 
-        let image = deduper::image::Image::from_path(image_path)?;
-        let hash = image.hash()?;
+        let image = deduper::image::Image::from_path(image_path).unwrap();
+
+        let hash = match image.hash() {
+            Ok(hash) => hash,
+            Err(_) => {
+                println!("Failed to get hash from {:?}", image_path);
+                return;
+            }
+        };
 
         // Convert the u64 hash to a byte array
         let serialised_hash = bincode::serialize(&hash).unwrap();
 
-        partition.insert(serialised_image_path, serialised_hash)?;
-        insert_count += 1;
-    }
+        {
+            let partition = partition.lock().unwrap();
+            partition
+                .insert(serialised_image_path, serialised_hash)
+                .unwrap();
+        }
 
-    Ok(insert_count)
+        {
+            let pb = pb.lock().unwrap();
+            pb.inc(1);
+        }
+    });
+
+    // pb.finish_with_message("Duplicate search complete");
+
+    Ok(())
 }
 
 // Find duplicate hashes in keystore values
